1dfcafe6412f6b048856e067c8f6b523da74c8a0
AlexMincu <minculescualex98@gmail.com>
2022-03-08 16:27:05 +0200

    started homepg; + 2 sections and style to home;

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
+/* eslint-disable no-undef */
 const express = require('express');
+const path = require('path');
+
 const app = express();
+
+const PORT = 8080;
 
 app.set('view engine', 'ejs');
 
+app.use('/assets', express.static(path.join(__dirname, 'assets')));
+
 app.get(['/', '/index', '/home'], (req, res) => {
-  res.render('pages/index');
+  res.status(200).render('pages/index');
 });
 
-app.listen(8080);
+app.listen(PORT);
 
-console.log('Server ON');
+console.log(`Server ON: http://localhost:${PORT}`);
