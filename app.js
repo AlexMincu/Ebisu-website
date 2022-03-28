6a34f1e9900c6d392ab325de015ca0d095170124
AlexMincu <minculescualex98@gmail.com>
2022-03-28 18:16:06 +0300

    Image resize & compression function

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 /* eslint-disable no-undef */
 const express = require('express');
+const fs = require('fs');
 const path = require('path');
+const sharp = require('sharp');
 
 const app = express();
 
@@ -17,3 +19,52 @@ app.get(['/', '/index', '/home'], (req, res) => {
 app.listen(PORT);
 
 console.log(`Server ON: http://localhost:${PORT}`);
+
+async function resizeImages(imgsFolder, ...dimensions) {
+  const imgsPath = path.join(__dirname, '/assets/images/', imgsFolder);
+  const compressedPath = path.join(imgsPath, 'compressed');
+
+  // if (fs.existsSync(compressedPath)) {
+  //   return;
+  // }
+
+  await fs.promises.mkdir(compressedPath, { recursive: true }, (err) =>
+    console.error(err)
+  );
+
+  fs.readdir(imgsPath, (err, files) => {
+    if (err) {
+      return console.error(err);
+    }
+
+    files.forEach((file) => {
+      if (file == 'compressed') return;
+
+      const [fileName, fileExt] = file.split('.');
+
+      if (fileExt == 'jpg' || fileExt == 'jpeg') {
+        dimensions.forEach(async (dimension) => {
+          await sharp(path.join(imgsPath, file))
+            .resize(dimension)
+            .toFormat('jpeg')
+            .jpeg({
+              quality: 80
+            })
+            .toFile(
+              path.join(
+                compressedPath,
+                fileName + '-' + dimension + '.' + 'jpeg'
+              )
+            )
+            .catch((err) => console.error(err));
+        });
+      }
+    });
+  });
+}
+
+resizeImages(path.join('homepage', 'cover'), 750, 1200, 1500);
+resizeImages('products', 600);
+resizeImages('categories', 650);
+resizeImages('books', 200);
+resizeImages('articles', 300);
--- assets/js/main.js
+++ assets/js/main.js
@@ -214,3 +214,12 @@ articlesCardsBtn.forEach((item) => {
 articlesBtn.addEventListener('click', () => {
   alert(`TODO Button Redirect to the Articles Page`);
 });
+
+function include(file) {
+  var script = document.createElement('script');
+  script.src = file;
+  script.type = 'text/javascript';
+  script.defer = true;
+
+  document.getElementsByTagName('head').item(0).appendChild(script);
+}
